d29d6cb128e0cd5905daf7cc282214cc6ee0ea55
PaulinaSzreder <paulina_szreder@wp.pl>
2018-03-08 11:35:34 +0100

    Zmiana projektu z Windows Forms na WPF. Wybieranie pliku i zapisywanie go do fileStream.

---
+++ Projekt_AES_WPF/Projekt_AES/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt_AES
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void chooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Możliwe że fileStream trzeba będzie zadeklarować wyżej, a tu zainicjalizować.
+                FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
+            }
+        }
+    }
+}
